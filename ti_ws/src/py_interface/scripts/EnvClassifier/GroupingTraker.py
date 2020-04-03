ef6aa2ae95ccc7e4c2dbbe4392a0771e0ac4e79e
Moonkey <zhangguanhua1997@163.com>
2020-04-03 16:00:32 +0800

    Add functions of classifier and prepare to add class marker.

--- ti_ws/src/py_interface/scripts/EnvClassifier/GroupingTraker.py
+++ ti_ws/src/py_interface/scripts/EnvClassifier/GroupingTraker.py
@@ -1,5 +1,6 @@
 from sklearn.cluster import DBSCAN
 from points_generator import PointsGenerator
+from ClassMarker import ClassMarker
 from utils import *
 
 
@@ -7,6 +8,7 @@ class GroupingTracker:
     def __init__(self):
         self.pc2 = None
         self.db = None
+        self.res_pc2 = None
         self.clusters_num = None
         self.clusters = []
         self.point_generator = PointsGenerator()
@@ -23,11 +25,11 @@ class GroupingTracker:
         return self.clusters
 
     def generate_points_per_cluster(self):
-        res_points = []
+        self.res_pc2 = []
         # TODO: Use ClassMarker to filter noise cluster and set marks
         for i in range(self.clusters_num):
-            res_points.extend(self.point_generator.generate(self.clusters[i]))
-        return res_points
+            self.res_pc2.extend(self.point_generator.generate(self.clusters[i]))
+        return self.res_pc2
 
 
     def show_clusters(self):
@@ -40,8 +42,10 @@ class GroupingTracker:
         plt.show()
 
     def show_generated_points(self):
-
-
+        if(self.res_pc2 is None):
+            self.generate_points_per_cluster()
+        plt.plot([p[0] for p in self.res_pc2], [p[1] for p in self.res_pc2], 'o')
+        plt.show()
 
 if __name__ == '__main__':
     import matplotlib
@@ -52,4 +56,4 @@ if __name__ == '__main__':
     gt = GroupingTracker()
     source_points = get_points_from_pcd('0.pcd')
     gt.pc_group(source_points)
-    gt.show_clusters()
+    gt.show_generated_points()
