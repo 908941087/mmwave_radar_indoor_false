1e03a679a83610471f79fa031eea136104c74632
Moonkey <zhangguanhua1997@163.com>
2020-04-08 18:20:28 +0800

    Modify mmwave output header.

--- ti_ws/src/ti_mmwave_rospkg/include/mmWave.h
+++ ti_ws/src/ti_mmwave_rospkg/include/mmWave.h
@@ -53,256 +53,127 @@
 enum MmwDemo_Output_TLV_Types
 {
     MMWDEMO_OUTPUT_MSG_NULL = 0,
-    /*! @brief   检测到的点数 */
-    /*! 点云数据 tlv_type == 1 */
-    MMWDEMO_OUTPUT_MSG_DETECTED_POINTS,
+    /*! @brief   List of detected points */
+            MMWDEMO_OUTPUT_MSG_DETECTED_POINTS,
 
-    /*! @brief   数据 */
-    MMWDEMO_OUTPUT_MSG_RANGE_PROFILE,
+    /*! @brief   Range profile */
+            MMWDEMO_OUTPUT_MSG_RANGE_PROFILE,
 
-    /*! @brief   噪声 */
-    MMWDEMO_OUTPUT_MSG_NOISE_PROFILE,
+    /*! @brief   Noise floor profile */
+            MMWDEMO_OUTPUT_MSG_NOISE_PROFILE,
 
-    /*! @brief  计算 azimuth  heatmap */
-    MMWDEMO_OUTPUT_MSG_AZIMUTH_STATIC_HEAT_MAP,
+    /*! @brief   Samples to calculate static azimuth  heatmap */
+            MMWDEMO_OUTPUT_MSG_AZIMUTH_STATIC_HEAT_MAP,
 
-    /*! @brief   速度、多普勒 */
-    MMWDEMO_OUTPUT_MSG_RANGE_DOPPLER_HEAT_MAP,
+    /*! @brief   Range/Doppler detection matrix */
+            MMWDEMO_OUTPUT_MSG_RANGE_DOPPLER_HEAT_MAP,
 
-    /*! @brief   Point Cloud - Array of detected points (range/angle/doppler) */
-    MMWDEMO_OUTPUT_MSG_POINT_CLOUD,
+    /*! @brief   Stats information */
+            MMWDEMO_OUTPUT_MSG_STATS,
 
-    /*! @brief   Target List - Array of detected targets (position, velocity, error covariance) */
-    MMWDEMO_OUTPUT_MSG_TARGET_LIST,
-
-    /*! @brief   Target List - Array of target indices */
-    MMWDEMO_OUTPUT_MSG_TARGET_INDEX,
-
-    /*! @brief   Classifier Output -- Array of target indices and tags */
-    MMWDEMO_OUTPUT_MSG_CLASSIFIER_OUTPUT,
-
-    /*! @brief   状态信息 */
-    MMWDEMO_OUTPUT_MSG_STATS,
-
-    /*! @brief   检测到的信息 */
-    MMWDEMO_OUTPUT_MSG_DETECTED_POINTS_SIDE_INFO,
+    /*! @brief   List of detected points side information */
+            MMWDEMO_OUTPUT_MSG_DETECTED_POINTS_SIDE_INFO,
 
     MMWDEMO_OUTPUT_MSG_MAX
 };
 
-enum SorterState{ READ_HEADER, 
+enum SorterState{ READ_HEADER,
     CHECK_TLV_TYPE,
-    READ_OBJ_STRUCT, 
-    READ_LOG_MAG_RANGE, 
-    READ_NOISE, 
-    READ_AZIMUTH, 
-    READ_DOPPLER, 
+    READ_OBJ_STRUCT,
+    READ_LOG_MAG_RANGE,
+    READ_NOISE,
+    READ_AZIMUTH,
+    READ_DOPPLER,
     READ_STATS,
     SWAP_BUFFERS,
     READ_SIDE_INFO};
 
-
-//struct MmwDemo_output_message_header_t
-//    {
-//        /*! brief   Version: : MajorNum * 2^24 + MinorNum * 2^16 + BugfixNum * 2^8 + BuildNum   */
-//        uint32_t    version;
-//
-//        /*! @brief   总包长度 */
-//        uint32_t    totalPacketLen;
-//
-//        /*! @brief   平台类型 */
-//        uint32_t    platform;
-//
-//        /*! @brief   帧数 */
-//        uint32_t    frameNumber;
-//
-//        /*! @brief   信息在时间戳中创建的时间 */
-//        uint32_t    timeCpuCycles;
-//
-//        /*! @brief   检测到的物体数 */
-//        uint32_t    numDetectedObj;
-//
-//        /*! @brief   TLV数 */
-//        uint32_t    numTLVs;
-//
-//        /*! @brief   主帧数 */
-//        uint32_t    subFrameNumber;
-//    };
-
-
 struct MmwDemo_output_message_header_t
 {
-    /*! @brief SW Version: : MajorNum * 2^24 + MinorNum * 2^16 + BugfixNum * 2^8 + BuildNum   */
+    /*! brief   Version: : MajorNum * 2^24 + MinorNum * 2^16 + BugfixNum * 2^8 + BuildNum   */
     uint32_t    version;
-
-    /*! @brief HW platform type */
-    uint32_t    platform;
-
-    /*! @brief Time in CPU cycles when the message was created, R4F CPU cycles */
-    uint32_t    timeCpuCycles;
 
     /*! @brief   Total packet length including header in Bytes */
     uint32_t    totalPacketLen;
 
+    /*! @brief   platform type */
+    uint32_t    platform;
+
     /*! @brief   Frame number */
     uint32_t    frameNumber;
 
-    /*! @brief   For Advanced Frame config, this is the sub-frame number in the range
-     * 0 to (number of subframes - 1). For frame config (not advanced), this is always
-     * set to 0. */
+    /*! @brief   Time in CPU cycles when the message was created. For XWR16xx: DSP CPU cycles, for XWR14xx: R4F CPU cycles */
+    uint32_t    timeCpuCycles;
+
+    /*! @brief   Number of detected objects */
+    uint32_t    numDetectedObj;
+
+    /*! @brief   Number of TLVs */
+    uint32_t    numTLVs;
+
+    /*! @brief   Sub-frame Number (not used with XWR14xx) */
     uint32_t    subFrameNumber;
-
-    /*! @brief Detection Layer Margins */
-    uint32_t    chirpProcessingMargin;
-    uint32_t    frameProcessingMargin;
-
-    /*! @brief Localization Layer Timing */
-    uint32_t    trackingProcessingTime;
-    uint32_t    uartSendingTime;
-
-    /*! @brief Number of TLVs in this message*/
-    uint16_t    numTLVs;
-    /*! @brief Header checksum */
-    uint16_t    checksum;
-
 };
 
+// Detected object structure for mmWave SDK 1.x and 2.x
 struct MmwDemo_DetectedObj
-    {
-        uint16_t   rangeIdx;     /*!< @brief 速度索引 */
-        uint16_t   dopplerIdx;   /*!< @brief 多普勒索引 */
-        uint16_t   peakVal;      /*!< @brief 峰值 */
-        float  x;             /*!< @brief ｘ坐标 */
-        float  y;             /*!< @brief y坐标 */
-        float  z;             /*!< @brief z 坐标 */
-    };
-
-/** FIXME: Add indoor_false_det data struct */
-/**
- * @brief
- *  Message for reporting detected objects from data path.
- *
- * @details
- *  The structure defines the message body for detected objects from from data path.
- */
-typedef struct MmwDemo_output_message_tl_t
 {
-    /*! @brief   TLV type */
-    uint32_t    type;
+    uint16_t   rangeIdx;     /*!< @brief Range index */
+    uint16_t   dopplerIdx;   /*!< @brief Dopler index */
+    uint16_t   peakVal;      /*!< @brief Peak value */
+    int16_t  x;             /*!< @brief x - coordinate in meters. Q format depends on the range resolution */
+    int16_t  y;             /*!< @brief y - coordinate in meters. Q format depends on the range resolution */
+    int16_t  z;             /*!< @brief z - coordinate in meters. Q format depends on the range resolution */
+};
 
-    /*! @brief   Length in bytes */
-    uint32_t    length;
-
-} MmwDemo_output_message_tl;
-
-/*!
- * @brief
- * Structure holds the message body for the  Point Cloud units
- *
- * @details
- * Reporting units for range, azimuth, and doppler
- */
-typedef struct MmwDemo_output_message_point_unit_t
-{
-    /*! @brief azimuth  reporting unit, in radians */
-    float		azimuthUnit;
-    /*! @brief Doppler  reporting unit, in m/s */
-    float		dopplerUnit;
-    /*! @brief range reporting unit, in m */
-    float		rangeUnit;
-    /*! @brief SNR  reporting unit, linear */
-    float       snrUnit;
-
-} MmwDemo_output_message_point_unit;
-
-/*!
- * @brief
- * Structure holds the message body to UART for the  Point Cloud
- *
- * @details
- * For each detected point, we report range, azimuth, and doppler
- */
-typedef struct MmwDemo_output_message_UARTpoint_t
-{
-    /*! @brief Detected point azimuth, in number of azimuthUnit */
-    int8_t		azimuth;
-    /*! @brief Detected point doppler, in number of dopplerUnit */
-    int8_t		doppler;
-    /*! @brief Detected point range, in number of rangeUnit */
-    uint16_t		range;
-    /*! @brief Range detection SNR, in number of snrUnit */
-    uint16_t       snr;
-
-} MmwDemo_output_message_UARTpoint;
-
-
-typedef struct MmwDemo_output_message_UARTpointCloud_t
-{
-    MmwDemo_output_message_tl       header;
-    MmwDemo_output_message_point_unit pointUint;
-    MmwDemo_output_message_UARTpoint    point[1];
-} MmwDemo_output_message_UARTpointCloud;
-
-
+// Detected object structures for mmWave SDK 3.x (DPIF_PointCloudCartesian_t and DPIF_PointCloudSideInfo_t)
 
 /**
  * @brief
- *  笛卡尔坐标系中定义的点信息数据结构
+ *  Point cloud definition in Cartesian coordinate system - floating point format
  *
  */
 typedef struct DPIF_PointCloudCartesian_t
 {
-    /*! @brief  x  */
+    /*! @brief  x - coordinate in meters */
     float  x;
 
-    /*! @brief  y  */
+    /*! @brief  y - coordinate in meters */
     float  y;
 
-    /*! @brief  z  */
+    /*! @brief  z - coordinate in meters */
     float  z;
 
-    /*! @brief  多普勒速度　*/
+    /*! @brief  Doppler velocity estimate in m/s. Positive velocity means target
+     *          is moving away from the sensor and negative velocity means target
+     *          is moving towards the sensor. */
     float    velocity;
 }DPIF_PointCloudCartesian;
 
 /**
  * @brief
- *  SNR信息以及噪声阈值
+ *  Point cloud side information such as SNR and noise level
  *
  */
 typedef struct DPIF_PointCloudSideInfo_t
 {
-    
+    /*! @brief  snr - CFAR cell to side noise ratio in dB expressed in 0.1 steps of dB */
     int16_t  snr;
 
-    
+    /*! @brief  y - CFAR noise level of the side of the detected cell in dB expressed in 0.1 steps of dB */
     int16_t  noise;
 }DPIF_PointCloudSideInfo;
 
-typedef struct UART_OutputPoint{
-    /*! @brief Detected point azimuth, in number of azimuthUnit */
-    int8_t		azimuth;
-    /*! @brief Detected point doppler, in number of dopplerUnit */
-    int8_t		doppler;
-    /*! @brief Detected point range, in number of rangeUnit */
-    uint16_t		range;
-    /*! @brief Range detection SNR, in number of snrUnit */
-    uint16_t       snr;
-};
-
 
 struct mmwDataPacket{
-        
-    MmwDemo_output_message_header_t header;
-    
-    uint16_t numObjOut;
-    
-    uint16_t xyzQFormat;  // only used for SDK 1.x and 2.x
-    
-    MmwDemo_DetectedObj objOut;   // only used for SDK 1.x and 2.x
 
-    MmwDemo_output_message_point_unit unitOut;
-    UART_OutputPoint pointOut;
+    MmwDemo_output_message_header_t header;
+
+    uint16_t numObjOut;
+
+    uint16_t xyzQFormat;  // only used for SDK 1.x and 2.x
+
+    MmwDemo_DetectedObj objOut;   // only used for SDK 1.x and 2.x
 
     DPIF_PointCloudCartesian_t newObjOut;  // used for SDK 3.x
     DPIF_PointCloudSideInfo_t  sideInfo;   // used for SDK 3.x
@@ -311,7 +182,6 @@ struct mmwDataPacket{
 const uint8_t magicWord[8] = {2, 1, 4, 3, 6, 5, 8, 7};
 
 #endif
-
 
 
 
