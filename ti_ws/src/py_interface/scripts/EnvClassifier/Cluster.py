9423fdd7327be8051203fcb2e763bf7119c769b8
RainmanJiang <distancein1997@gmail.com>
2020-04-30 15:06:34 +0800

    temp commit2

--- ti_ws/src/py_interface/scripts/EnvClassifier/Cluster.py
+++ ti_ws/src/py_interface/scripts/EnvClassifier/Cluster.py
@@ -1,3 +1,7 @@
+import numpy as np
+from scipy.spatial import ConvexHull
+from sympy import Point, Polygon
+
 class Cluster(object):
     
     def __init__(self, id, points):
@@ -7,7 +11,7 @@ class Cluster(object):
         self.info = {}
 
     def getCenter(self):
-        pass
+        return [float(np.average([p[0] for p in self.points])), float(np.average([p[1] for p in self.points]))]
 
     def getPoints(self):
         return self.points
@@ -15,8 +19,14 @@ class Cluster(object):
     def getDensity(self):
         pass
 
+    def getConvexHull(self):
+        if self.hull is None:
+            points = np.array(self.points).reshape(-1, 2)
+            self.hull = ConvexHull(points)
+        return self.hull
+
     def getArea(self):
-        pass
+        
 
     def show(self, plt):
         plt.scatter([p[0] for p in self.points], [p[1] for p in self.points], c='r', s=1)
--- ti_ws/src/py_interface/scripts/EnvClassifier/Entity.py
+++ ti_ws/src/py_interface/scripts/EnvClassifier/Entity.py
@@ -4,7 +4,7 @@ class Entity(object):
 
     def __init__(self, id):
         self.entity_id = id
-        self.generated = False
+        self.enhanced = False
 
     def show(self, plt):
         pass
@@ -21,12 +21,15 @@ class Entity(object):
     def getInfo(self):
         pass
 
+    def isEnhanced(self):
+        return self.enhanced
+
 
 class Wall(Entity):
 
     def __init__(self, id):
         self.entity_id = id
-        self.generated = False
+        self.enhanced = False
         self.width = None
         self.length = None
 
@@ -39,12 +42,25 @@ class Wall(Entity):
     def show(self, plt):
         pass
 
+    def enhance(self):
+        pass
+
+    def getWidth(self):
+        pass
+
+    def getLength(self):
+        pass
+
 
 class Furniture(Entity):
 
     def __init__(self, id):
         self.entity_id = id
         self.type = None
+        self.is_enhanced = False
+
+    def enhance(self):
+        pass
 
 
 class Door(Entity):
@@ -56,3 +72,39 @@ class Door(Entity):
     def __init__(self, id):
         self.entity_id = id
         self.state = DoorState.CLOSED
+        self.is_enhanced = False
+
+    def getState(self):
+        return self.state
+
+    def getLocation(self):
+        pass
+
+    def getCenter(self):
+        pass
+
+    def enhance(self):
+        pass
+
+
+class Noise(Entity):
+
+    def __init__(self, id):
+        self.entity_id = id
+        self.center = None
+        self.is_enhanced = False
+
+    def getCenter(self):
+        pass
+
+    def enhance(self):
+        pass
+
+    def getNaiveCenter(self):
+        pass
+
+    def enhance(self):
+        pass
+
+    def getInfo(self):
+        pass
