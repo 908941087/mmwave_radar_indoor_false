08705c9f670fc807ef4889f687b35970c1be395b
RainmanJiang <distancein1997@gmail.com>
2020-05-01 11:08:04 +0800

    Added LiDAR SDK.

--- ti_ws/build/ydlidar_ros/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jzj/easy_ti_ws/src/ydlidar_ros
-# Build directory: /home/jzj/lab/radar/mmwave_radar_indoor_false/ti_ws/build/ydlidar_ros
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
