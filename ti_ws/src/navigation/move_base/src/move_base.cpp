26e5e3971d4f10f90cbe4dc1b5865e9ed656ca15
DistanceJiang <distancein1997@gmail.com>
2020-07-31 19:25:38 +0800

    Modified move_base.cpp

--- ti_ws/src/navigation/move_base/src/move_base.cpp
+++ ti_ws/src/navigation/move_base/src/move_base.cpp
@@ -1010,23 +1010,18 @@ namespace move_base {
             as_->setAborted(move_base_msgs::MoveBaseResult(), "Failed to find a valid control. Even after executing recovery behaviors.");
           }
           else if(recovery_trigger_ == PLANNING_R){
-            ROS_ERROR("Aborting because a valid plan could not be found. Even after executing all recovery behaviors. Added this tail.");
-            ROS_ERROR("This is a message.");
-            std_msgs::Int8 abort_mess;
-            abort_mess.data = 1;
-            abort_pub.publish(abort_mess);
-            ROS_ERROR("Abortion message sent.");
+            ROS_ERROR("Aborting because a valid plan could not be found. Even after executing all recovery behaviors.");
             as_->setAborted(move_base_msgs::MoveBaseResult(), "Failed to find a valid plan. Even after executing recovery behaviors.");
           }
           else if(recovery_trigger_ == OSCILLATION_R){
             ROS_ERROR("Aborting because the robot appears to be oscillating over and over. Even after executing all recovery behaviors");
-            std_msgs::Int8 abort_mess;
-            abort_mess.data = 1;
-            abort_pub.publish(abort_mess);
-            ROS_ERROR("Abortion message sent.");
             as_->setAborted(move_base_msgs::MoveBaseResult(), "Robot is oscillating. Even after executing recovery behaviors.");
           }
           resetState();
+          std_msgs::Int8 abort_mess;
+          abort_mess.data = 1;
+          abort_pub.publish(abort_mess);
+          ROS_ERROR("Abortion message sent.");
           return true;
         }
         break;
